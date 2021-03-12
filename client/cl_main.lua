751141d13195926233bb1450306599fe1f3f9599
Christopher <onlygamernerd@gmail.com>
2021-03-13 00:44:27 +0100

    feat(client): exports

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,6 +1,22 @@
 -- Create a export that you can use use client side
 
-local CreateMenu = function()
-  -- this will create a new menu
-  SendMessage('createMenu', 'some array or table')
-end
+-- should be a table[array]
+exports('CreateSelectMenu', function(elements)
+  print('Creating menu...')
+
+  SendMessage('setData', elements)
+end)
+
+RegisterNUICallback('CloseSelectMenu', function(data, cb)
+  SendMessage('setVisibility', false)
+  
+  cb()
+end)
+
+-- allows creation menu from server side
+RegisterNetEvent('CreateSelectMenu')
+AddEventHandler('CreateSelectMenu', function(elements)
+  print('Creating menu...')
+
+  SendMessage('setData', elements)
+end)
