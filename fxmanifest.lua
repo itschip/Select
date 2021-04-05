2520078aa66e99398b16ed3d820390f2ed1e98cd
Christopher <onlygamernerd@gmail.com>
2021-04-05 20:00:51 +0200

    feat: fxmanifest nui

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+game "gta5"
+
+client_script "resources/dist/client/*.client.js"
+
+ui_page "menu/public/index.html"
+
+files {
+    'menu/public/index.html',
+    'menu/public/build/bundle.js',
+    'menu/public/build/bundle.css'
+}
