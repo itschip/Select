fe94474f8f7bd2434d5db9bdf87aec5b5c7286a9
Christopher <onlygamernerd@gmail.com>
2021-04-10 16:48:49 +0200

    Menu comp

--- menu/src/App.tsx
+++ menu/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { useItems, useVisibility } from './context/MenuProvider';
+import Menu from './Menu/Menu';
 
 function App() {
   const { visibility } = useVisibility();
-  const { items } = useItems();
+  const items = useItems();
 
   return <div className="App">{visibility && <Menu items={items} />}</div>;
 }
--- menu/src/Menu/Menu.tsx
+++ menu/src/Menu/Menu.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { IMenuItem } from '../typings/context';
+import styles from './Menu.module.scss';
 
 export default function Menu({ items }: { items: IMenuItem[] }) {
   return (
-    <div>
+    <div className={styles.appWrapper}>
       <div>
         {items.map((item) => (
           <div>{item.title}</div>
