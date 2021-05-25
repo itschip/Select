68cb3d6f99cb7ea68bb5b5856b5f7bcabb5c4796
Christopher <onlygamernerd@gmail.com>
2021-05-25 04:12:10 +0200

    update menu item'

--- menu/src/Menu/Menu.tsx
+++ menu/src/Menu/Menu.tsx
@@ -3,12 +3,17 @@ import { IMenuItem } from '../typings/context';
 import styles from './Menu.module.scss';
 
 export default function Menu({ items }: { items: IMenuItem[] }) {
+
+  const handleSelectedItem = (item: IMenuItem) => {
+    console.log(item.title)
+  }
+
   return (
     <div className={styles.appWrapper}>
       <h1>hello</h1>
       <div className={styles.menu}>
         {items.map((item) => (
-          <div key={item.id} className={styles.item} onClick={() => console.log(item.title)}>
+          <div key={item.id} className={styles.item} onClick={() => handleSelectedItem(item)}>
             <h2>{item.title}</h2>
             <p>{item.description}</p>
           </div>
